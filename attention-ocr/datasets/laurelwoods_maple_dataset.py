3cbfc1cdacc9eb6d3df3f3b128f3156ee8452f09
Laurelwoods <laurelwoods0102@naver.com>
2020-10-17 19:34:52 +0900

    seg first train

---
+++ attention-ocr/datasets/laurelwoods_maple_dataset.py
@@ -0,0 +1,51 @@
+import os
+
+from datasets import fsns
+
+DEFAULT_DATASET_DIR = os.path.join(os.path.dirname(__file__), 'laurelwoods-maple-dataset')
+
+DEFAULT_CONFIG = {
+    'name':
+        'laurelwoods-maple',
+    'splits': {
+        'train': {
+            'size': 121,
+            'pattern': 'tfexample_train*'
+        },
+        'test': {
+            'size': 121,
+            #'pattern': 'tfexample_test*'
+            'pattern': 'tfexample_train*'
+        }
+    },
+    'charset_filename':
+        'word_bag_3.txt',
+    'image_shape': (300, 300, 3),
+    'num_of_views':
+        1,
+    'max_sequence_length':
+        320,
+    'null_code':
+        0,
+    'items_to_descriptions': {
+        'image':
+            'A [300 x 200 x 3] color image.',
+        'label':
+            'Characters codes.',
+        'text':
+            'A unicode string.',
+        'length':
+            'A length of the encoded text.',
+        'num_of_views':
+            'A number of different views stored within the image.'
+    }
+}
+
+
+def get_split(split_name, dataset_dir=None, config=None):
+  if not dataset_dir:
+    dataset_dir = DEFAULT_DATASET_DIR
+  if not config:
+    config = DEFAULT_CONFIG
+
+  return fsns.get_split(split_name, dataset_dir, config)
---
+++ dataset_preprocessing/segmentation/segmentation_preprocessing_properties.md
@@ -0,0 +1,13 @@
+# Segmentation Preprocessing Properties
+
+1. Padding for image (Can be updated later):
+    - target_width = 300
+    - target_height = 800
+2. Classes : 
+    1. class_0 : 일반 옵션  
+    2. class_1 : 잠재 옵션  
+    3. class_2 : 에디셔널 옵션
+3. Ignore : 
+    1. 소울
+    2. 모루
+4. 
